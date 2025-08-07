6e28d4cb9f291d361d712e8e3378c41861560f17
JamwalSunny <sjamwal-c@beesolvertechnology.com>
2025-08-07 22:06:28 +0530

    code new

---
+++ Test1.spec.js
@@ -0,0 +1,11 @@
+describe('Dummy Test', () => {
+  it('should visit a page and check the title', () => {
+    cy.visit('https://example.cypress.io')
+    cy.title().should('include', 'Cypress')
+  })
+
+  it('should check if element exists', () => {
+    cy.visit('https://example.cypress.io')
+    cy.get('h1').should('contain', 'Kitchen Sink')
+  })
+})
